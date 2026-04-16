2f9a7932c51d74628cca86b375a8be251c9f1c4e
Maria Ramez <mramez2003@gmail.com>
2026-04-16 10:37:15 +0200

    your message

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { loginAdmin, registerAdmin } = require("../controllers/authController");
+const { registerAdmin, loginAdmin } = require("../controllers/authController");
 
+router.post("/register", registerAdmin);
 router.post("/login", loginAdmin);
 
 module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+console.log("Mongo URI:", process.env.MONGO_URI);
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
